f7b102a7693d61f9f22396cf9520068992ae3a28
Milan Savaliya <friendlymsavaliya@gmail.com>
2020-02-05 10:51:43 +0530

    Initial Commit

---
+++ src/main/java/com/milan/mvcreversestring/MvcReverseStringApplication.java
@@ -0,0 +1,13 @@
+package com.milan.mvcreversestring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcReverseStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcReverseStringApplication.class, args);
+	}
+
+}
